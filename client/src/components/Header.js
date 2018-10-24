aba276fb4787d263f42e2db51c6f0b4047354a01
Daniel Newman <dwnewman78@gmail.com>
2018-10-24 08:50:05 -0500

    Added payment processing to the frontend

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,24 +1,31 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import Payments from './Payments'
 
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
       case null:
-        return;
+        return
       case false:
         return (
           <li>
             <a href="/auth/google">Login With Google</a>
           </li>
-        );
+        )
       default:
-        return (
-          <li>
+        return [
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="3" style={{margin: '0 10px'}}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="2">
             <a href="/api/logout">Logout</a>
-          </li>
-        );
+          </li>,
+        ]
     }
   }
 
@@ -27,7 +34,7 @@ class Header extends Component {
       <nav>
         <div className="nav-wrapper">
           <Link
-            to={this.props.auth ? "/surveys" : "/"}
+            to={this.props.auth ? '/surveys' : '/'}
             className="left brand-logo"
           >
             Emaily
@@ -35,12 +42,12 @@ class Header extends Component {
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
-    );
+    )
   }
 }
 
-function mapStateToProps({ auth }) {
-  return { auth };
+function mapStateToProps({auth}) {
+  return {auth}
 }
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(Header)
